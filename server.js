f57f256a2b80cc39c5006ac6a224ca9e7e4ae94a
Wiam Nasr <wiamnasr@gmail.com>
2022-02-02 12:46:14 +0000

    done including stretch goal

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
 
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "wiam",
+  password: "hello123",
+  database: "cyf_ecommerce",
+});
+
 const cors = require("cors");
 
 const app = express();
@@ -7,6 +17,27 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+
+app.get("/products", (req, res) => {
+  pool.query(
+    "SELECT products.product_name, product_availability.unit_price, suppliers.supplier_name FROM products INNER JOIN product_availability ON products.id = product_availability.prod_id INNER JOIN suppliers ON product_availability.supp_id = suppliers.id;",
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.get("/suppliers", (req, res) => {
+  pool.query("SELECT * FROM suppliers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.get("/customers", (req, res) => {
+  pool.query("SELECT * FROM customers", (error, result) => {
+    res.json(result.rows);
+  });
+});
 
 app.get("/", (req, res) => {
   return res
